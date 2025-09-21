54c6fec07c1f08ea2312856d66e77f1ceab82c5b
DIVYaTRIPATHI12 <divyanshtripathi48762@gmail.com>
2025-09-22 02:32:21 +0530

    Add files via upload
    This project is a dynamic, client-side web application that demonstrates real-time content filtering. Built using HTML, CSS, and vanilla JavaScript, it allows users to filter a collection of Movies by clicking on different category buttons. The core functionality relies on JavaScript's .'filter()' array method to efficiently match and display content based on user input. The application features a clean, responsive user interface and is an excellent example of interactive web design without a backend.

---
+++ Assignment 7/script.js
@@ -0,0 +1,55 @@
+const movies = [
+  { title: "Esha Murder Karmphal", genre: "Action", image: 'image-1.jpg' },
+  { title: "Top Gear", genre: "Comedy", image: 'image-2.jpg' },
+  { title: "Devils Double Next Level", genre: "Drama", image: 'image-3.avif' },
+  { title: "X-Men 9 Apocalypse", genre: "Adventure", image: 'image-4.jpeg' },
+  { title: "Dragon Curse The Hidden Treasure", genre: "Romance", image: 'image-5.jpg' },
+  { title: "Mahavatar Narsimha", genre: "Thriller", image: 'image-6.jpeg' },
+  { title: "Kalki", genre: "Fantasy", image: 'image-7.jpeg' },
+  { title: "Tumbbad", genre: "Horror", image: 'image-8.webp' }
+];
+
+const movieList = document.getElementById('movieList');
+const searchInput = document.getElementById('searchInput');
+const tags = document.querySelectorAll('.tag');
+
+let currentGenre = 'All'; 
+
+function displayMovies(movieArray) {
+  movieList.innerHTML = '';
+  movieArray.forEach(movie => {
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-card');
+    movieCard.innerHTML = `
+      <img src="${movie.image}" alt="${movie.title}">
+      <h3>${movie.title}</h3>
+    `;
+    movieList.appendChild(movieCard);
+  });
+}
+
+function filterMovies() {
+  const searchText = searchInput.value.toLowerCase();
+
+  const filtered = movies.filter(movie => {
+    const matchesGenre = currentGenre === 'All' || movie.genre === currentGenre;
+    const matchesSearch = movie.title.toLowerCase().includes(searchText);
+    return matchesGenre && matchesSearch;
+  });
+
+  displayMovies(filtered);
+}
+
+displayMovies(movies);
+searchInput.addEventListener('input', filterMovies);
+
+// for filtering Genre
+tags.forEach(tag => {
+  tag.addEventListener('click', () => {
+    tags.forEach(t => t.classList.remove('active'));
+    tag.classList.add('active');
+    currentGenre = tag.getAttribute('data-genre'); 
+    filterMovies();
+  });
+});
+
